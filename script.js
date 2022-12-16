660d8db57f7238a9f9181404479202fbd508ed55
GustavoNery88 <gusta123sam@gmail.com>
2022-12-16 09:54:49 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function desenvolvedores() {
+    let mostrar = document.getElementById("lerMais")
+    if (mostrar.style.display === "inline") {
+        mostrar.style.display = "none"
+    } else {
+        mostrar.style.display = "inline"
+    }
+}
+
+let janelaDaSegundaFeira = document.getElementById("modal-SegundaFeira")
+function butaoSegundaFeira() {
+    janelaDaSegundaFeira.style.display = "flex"
+}
+
+function botaoFecharSegundaFeira() {
+    janelaDaSegundaFeira.style.display = "none"
+}
+
+let janelaDaTercaFeira = document.getElementById("modal-TercaFeira")
+function butaoTercaFeira() {
+    janelaDaTercaFeira.style.display = "flex"
+}
+
+function botaoFecharTercaFeira() {
+    janelaDaTercaFeira.style.display = "none"
+}
+
+let janelaDaQuartaFeira = document.getElementById("modal-QuartaFeira")
+function butaoQuartaFeira() {
+    janelaDaQuartaFeira.style.display = "flex"
+}
+
+function botaoFecharQuartaFeira() {
+    janelaDaQuartaFeira.style.display = "none"
+}
+
+let janelaDaQuintaFeira = document.getElementById("modal-QuintaFeira")
+function butaoQuintaFeira() {
+    janelaDaQuintaFeira.style.display = "flex"
+}
+
+function botaoFecharQuintaFeira() {
+    janelaDaQuintaFeira.style.display = "none"
+}
+
+let janelaDaSextaFeira = document.getElementById("modal-SextaFeira")
+function butaoSextaFeira() {
+    janelaDaSextaFeira.style.display = "flex"
+}
+
+function botaoFecharSextaFeira() {
+    janelaDaSextaFeira.style.display = "none"
+}
